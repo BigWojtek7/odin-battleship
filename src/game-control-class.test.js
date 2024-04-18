5e5aad328b3a735fe29a63ef1c4898607e3adb62
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-04-18 15:42:17 +0200

    Add more tests to game-control-class

--- src/game-control-class.test.js
+++ src/game-control-class.test.js
@@ -1,8 +1,20 @@
-import GameControl from "./game-control-class"
+import GameControl from "./game-control-class";
 
 describe("game controller", () => {
-  const game = new GameControl
+  const game = new GameControl();
   test("switch turns", () => {
-    expect(game.switchTurns())
-  })
-})
+    expect(game.activePlayer).toBe(game.player1);
+  });
+  test("switch turns", () => {
+    game.switchTurns();
+    expect(game.activePlayer).toBe(game.player2);
+  });
+
+  test("switch boards", () => {
+    expect(game.activeBoard).toStrictEqual(game.boardPlayer2);
+  });
+  test("switch boards", () => {
+    game.switchTurns();
+    expect(game.activeBoard).toStrictEqual(game.boardPlayer1);
+  });
+});
